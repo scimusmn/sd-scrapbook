cdb311e262c00add564e26a66a85211e12aa9407
bryan kennedy <bkennedy@smm.org>
2014-09-11 22:12:31 -0500

    Drawing some basic shapes with D3

--- client/views/locations/locations.js
+++ client/views/locations/locations.js
@@ -19,18 +19,27 @@ Template.locations.rendered = function () {
      * Arbitrary D3 test code to learn the basics of drawing
      * SVG items on the canvas
      */
-    var dataset = [ 5, 10, 15, 20, 25 ];
+    var dataset = [];
+
+    for (var i = 0; i < 25; i++) {           //Loop 25 times
+        var newNumber = Math.round(Math.random() * 30);
+        dataset.push(newNumber);             //Add new number to array
+    }
+
     d3.select(".container").
-        selectAll("div").
+        selectAll("div.d3").
         data(dataset).
         enter().
         append("div").
         attr('class', 'bar').
         style("height", function(d) {
-            return d + "px"
+            return (d * 5) + "px"
+        }).
+        text(function(d) {
+            return d;
         });
 };
 
 Template.locations.events({
-    //
+    // Respond to events
 });
