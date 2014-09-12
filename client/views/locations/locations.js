504e532623fd3702840f619e51d1dc0fb9a2aff4
bryan kennedy <bkennedy@smm.org>
2014-09-11 21:31:23 -0500

    Mapping data within the page bounds

--- client/views/locations/locations.js
+++ client/views/locations/locations.js
@@ -7,17 +7,23 @@ Code related to the locations template
 /+ ---------------------------------------------------- */
 
 Template.locations.created = function () {
-  //
+    //
 };
 
 Template.locations.helpers({
-  //
+    //
 });
 
 Template.locations.rendered = function () {
-    d3.select("body").append("p").text("New paragraph!");
+    var dataset = [ 5, 10, 15, 20, 25 ];
+    d3.select(".container").
+        selectAll("p").
+        data(dataset).
+        enter().
+        append("p").
+        text(function(d) { return d; });
 };
 
 Template.locations.events({
-  //
+    //
 });
