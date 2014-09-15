d5fe07e696d54a13d66aafb4b9c650e3fb09f2a2
bryan kennedy <bkennedy@smm.org>
2014-09-15 16:46:10 -0500

    Small map focused on Arizona with Web Merc
    This gives me the basic elements that I need in a map. A web mercator
    projection focused on the center of Arizona. Now that I've got this
    nailed down I can figure out how to center the map on the same location
    as my PNG map export.

--- client/views/locations/locations.js
+++ client/views/locations/locations.js
@@ -25,27 +25,47 @@ Template.locations.rendered = function () {
         var newNumber = Math.round(Math.random() * 30);
         dataset.push(newNumber);             //Add new number to array
     }
-    var width = 960,
-    height = 1160;
+    var width = 600,
+    height = 300;
 
-    var projection = d3.geo.albers()
-    .center([-111, 34.2])
-    .rotate([4.4, 0])
-    .parallels([50, 60])
-    .scale(1200 * 5)
-    .translate([width / 2, height / 2]);
+    /**
+     * Projection centered on Arizon, but for some reason Arizona is turned
+     * 90 degrees on its side.
+     */
+    //var projection = d3.geo.albers()
+    //.center([-111, 34.2])
+    //.rotate([0, 0])
+    //.parallels([50, 60])
+    //.scale(1200 * 5)
+    //.translate([width / 2, height / 2]);
+
+    /**
+     * Albers - focused on the continental USA
+     */
+    //var projection = d3.geo.albersUsa()
+    //.scale(1280)
+    //.translate([width / 2, height / 2]);
+
+    /**
+     * Web mercator focused on the world
+     */
+    var projection = d3.geo.mercator()
+        .scale(1000)
+        .center([-111, 34.2])
+        .translate([width / 2, height / 2])
+        .precision(.1);
 
     var path = d3.geo.path()
-    .projection(projection);
+        .projection(projection);
 
     var svg = d3.select(".container").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+        .attr("width", width)
+        .attr("height", height);
 
     d3.json("/data/arizona.json", function(error, arizona) {
         svg.append("path")
-        .datum(topojson.feature(arizona, arizona.objects.arizona))
-        .attr("d", path);
+            .datum(topojson.feature(arizona, arizona.objects.arizona))
+            .attr("d", path);
     });
 };
 
