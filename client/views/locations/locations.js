8ac663148950a9eae19f8997c12ef015959252e5
bryan kennedy <bkennedy@smm.org>
2014-09-15 14:01:33 -0500

    Adding some basic SVG drawing tests
    Mostly experimenting with how to add circles and squares across the map.

--- client/views/locations/locations.js
+++ client/views/locations/locations.js
@@ -26,6 +26,35 @@ Template.locations.rendered = function () {
         dataset.push(newNumber);             //Add new number to array
     }
 
+    d3.json("/data/ocean.geojson", function(error, ocean) {
+        if (error) return console.error(error);
+        console.log(ocean);
+        svg.append("path").
+            datum(topojson.feature(ocean, ocean.objects.subunits)).
+            attr("d", d3.geo.path().projection(d3.geo.mercator()));
+    });
+
+    var w = 500;
+    var h = 50;
+    var svg = d3.select(".container").
+        append("svg").
+        attr("width", w).
+        attr("height", h);
+
+    var circles = svg.selectAll("circle").
+        data(dataset).
+        enter().
+        append("circle");
+
+    circles.
+        attr("cx", function(d, i) {
+            return (i * 50) + 25;
+        }).
+        attr("cy", h/2).
+        attr("r", function(d) {
+            return d;
+        });
+
     d3.select(".container").
         selectAll("div.d3").
         data(dataset).
