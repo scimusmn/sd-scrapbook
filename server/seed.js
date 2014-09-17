562735aef6b616bb2fadf268f6cc5da1f165d300
bryan kennedy <bkennedy@smm.org>
2014-09-17 13:31:54 -0500

    Switching body to description
    This matches the asset database that SD will be using.

--- server/seed.js
+++ server/seed.js
@@ -11,14 +11,14 @@ if (Locations.find().count() === 0) {
 
   Locations.insert({
     title: "Eridanus",
-    body: "Eridanus is a constellation. It is represented as a river; its name is the Ancient Greek name for the Po River.",
+    description: "Eridanus is a constellation. It is represented as a river; its name is the Ancient Greek name for the Po River.",
     latitude: "32.715",
     longitude: "-117.1625"
   });
 
   Locations.insert({
     title: "Cassiopeia",
-    body: "Cassiopeia is a constellation in the northern sky, named after the vain queen Cassiopeia in Greek mythology, who boasted about her unrivalled beauty.",
+    description: "Cassiopeia is a constellation in the northern sky, named after the vain queen Cassiopeia in Greek mythology, who boasted about her unrivalled beauty.",
     latitude: "34.05",
     longitude: "-118.25"
   });
