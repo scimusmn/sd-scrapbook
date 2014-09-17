140421d1a3d6b4bb8a80183849988de1e7ea8eba
bryan kennedy <bkennedy@smm.org>
2014-09-17 17:14:45 -0500

    Adding new images and data to the seed

--- server/seed.js
+++ server/seed.js
@@ -6,24 +6,26 @@ Fill in the app with dummy data if database is empty.
 
 /+ ---------------------------------------------------- */
 
-// Fixture data
+/**
+ * Locations
+ */
 if (Locations.find().count() === 0) {
 
-  Locations.insert({
-    title: "Eridanus",
-    description: "Eridanus is a constellation. It is represented as a river; its name is the Ancient Greek name for the Po River.",
-    latitude: "32.715",
-    longitude: "-117.1625"
+  var sanDiegoCst = Locations.insert({
+    title: "San Diego",
+    description: "San Diego is a city along the coast.",
+    latitude: "32.551019",
+    longitude: "-117.111768"
   });
 
-  Locations.insert({
-    title: "Cassiopeia",
-    description: "Cassiopeia is a constellation in the northern sky, named after the vain queen Cassiopeia in Greek mythology, who boasted about her unrivalled beauty.",
-    latitude: "34.05",
-    longitude: "-118.25"
+  var losAngeles = Locations.insert({
+    title: "Los Angeles",
+    description: "Los Angeles is a city",
+    latitude: "34.0218628",
+    longitude: "-118.4804206"
   });
 
-  Locations.insert({
+  var anzaBorrego = Locations.insert({
     title: "Anza-Borrego Desert",
     description: "Anza-Borrego Desert State Park (ABDSP) is a state park located within the Colorado Desert of southern California, United States.",
     latitude: "33.100492",
@@ -33,14 +35,47 @@ if (Locations.find().count() === 0) {
 
 }
 
+/**
+ * Images
+ */
 if (Images.find().count() === 0) {
 
-  Images.insert({
+  var ds01001 = Images.insert({
     title: "Fieldtrip",
+    parentLocation: anzaBorrego,
     description: "Beaty, guide, and Mrs. Abbott among ocotillos during San Diego Natural History Museum trip to Borrego Valley and Thousand Palm Canyon.",
     latitude: "33.100492",
-    longitude: "-116.301327"
+    longitude: "-116.301327",
+    date: '1927'
+  });
 
+  var ds01002 = Images.insert({
+    title: "Coyote Creek",
+    parentLocation: anzaBorrego,
+    description: "Beaty and Mrs. Abbott on edge of Coyote Creek during San Diego Natural History Museum trip to Borrego Valley and Thousand Palm Canyon.",
+    latitude: "33.100492",
+    longitude: "-116.301327",
+    date: '1927'
+  });
+
+  var ds13004 = Images.insert({
+    title: "",
+    parentLocation: sanDiegoCst,
+    description: "Tijuana Estuary",
+    latitude: "32.551019",
+    longitude: "-117.111768",
+    date: '2014-Apr-11',
+    photographer: 'Jim Melli'
+  });
+
+  var ds19001Then = Images.insert({
+    title: "Alluvial Bluffs along the Pacific Coast north of Santa Monica Canyon.",
+    parentLocation: losAngeles,
+    description: "Alluvial Bluffs along the Pacific Coast north of Santa Monica Canyon. Pacific Ocean in background. Los Angeles County, Calfornia. ca 1904.",
+    latitude: "33.100492",
+    longitude: "-116.301327",
+    date: '1927',
+    photographer: 'Mendenhall, W.C.'
   });
 
 }
