cb2f43d3660371fd9bcd90e71fdac0f6db2453d2
bryan kennedy <bkennedy@smm.org>
2014-09-16 21:46:18 -0500

    Adding fake location data to the seed
    This will make it possible to test maping with the lat long values.

--- server/seed.js
+++ server/seed.js
@@ -11,17 +11,23 @@ if (Locations.find().count() === 0) {
 
   Locations.insert({
     title: "Eridanus",
-    body: "Eridanus is a constellation. It is represented as a river; its name is the Ancient Greek name for the Po River."
+    body: "Eridanus is a constellation. It is represented as a river; its name is the Ancient Greek name for the Po River.",
+    latitude: "32.715",
+    longitude: "-117.1625"
   });
 
   Locations.insert({
     title: "Cassiopeia",
-    body: "Cassiopeia is a constellation in the northern sky, named after the vain queen Cassiopeia in Greek mythology, who boasted about her unrivalled beauty."
+    body: "Cassiopeia is a constellation in the northern sky, named after the vain queen Cassiopeia in Greek mythology, who boasted about her unrivalled beauty.",
+    latitude: "34.05",
+    longitude: "-118.25"
   });
 
   Locations.insert({
     title: "Scorpius",
-    body: "Scorpius, sometimes known as Scorpio, is one of the constellations of the zodiac."
+    body: "Scorpius, sometimes known as Scorpio, is one of the constellations of the zodiac.",
+    latitude: "33.05",
+    longitude: "-118.25"
   });
 
 }
