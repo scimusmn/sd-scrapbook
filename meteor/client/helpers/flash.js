ec435d667302644e14690d990357f0e74d76d752
bryan kennedy <bkennedy@smm.org>
2014-10-17 16:06:39 -0500

    Style cleanup

--- meteor/client/helpers/flash.js
+++ meteor/client/helpers/flash.js
@@ -2,10 +2,10 @@
 Messages = new Meteor.Collection(null);
 
 flash = function (message, messageType) {
-	var type = typeof messageType === 'undefined' ? 'success' : messageType;
-	Messages.remove({});
-	Messages.insert({
-		message: message,
-		type: type
-	});
-}
+    var type = typeof messageType === 'undefined' ? 'success' : messageType;
+    Messages.remove({});
+    Messages.insert({
+        message: message,
+        type: type
+    });
+};
