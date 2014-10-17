f16d99f6c49201a39ee314161c14de9c7555d06d
bryan kennedy <bkennedy@smm.org>
2014-10-17 16:03:21 -0500

    Style cleanup

--- meteor/client/views/common/header.js
+++ meteor/client/views/common/header.js
@@ -1,14 +1,14 @@
 Template.header.helpers({
-  messages: function () {
-    return Messages.find();
-  },
-  isLoggedIn: function () {
-    return !!Meteor.user();
-  }
-})
+    messages: function () {
+        return Messages.find();
+    },
+    isLoggedIn: function () {
+        return !!Meteor.user();
+    }
+});
 
 Template.header.events({
-  'click .log-out': function () {
-    Meteor.logout();
-  }
-})
+    'click .log-out': function () {
+        Meteor.logout();
+    }
+});
