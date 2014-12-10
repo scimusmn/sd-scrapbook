05948be26547e3d9d89f71d8b517a0997371e3a8
bryan kennedy <bkennedy@smm.org>
2014-12-10 11:41:56 -0600

    Removing dev strings for date output
    These were only here for debugging in Excel

--- bin/process-data.py
+++ bin/process-data.py
@@ -198,9 +198,9 @@ for row in csv.reader(input):
                            str(int(yearlist[1])))
 
         # row.insert(8, str(str(year) + '-' + str(month) + '-' + str(day)))
-        row.insert(8, "a" + date_string)
+        row.insert(8, date_string)
 
-        row.insert(9, "z" + iso_date)
+        row.insert(9, iso_date)
 
         writer.writerow(row)
 input.close()
