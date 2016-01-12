ee54e07b903cb69114743f80a4fd295f8eae1b84
Trygve <tnordberg@smm.org>
2016-01-12 17:42:57 -0600

    Added autoform and collection2 package.

--- meteor/client/views/admin/adminLocation.js
+++ meteor/client/views/admin/adminLocation.js
@@ -1,6 +1,8 @@
 /**
  * Template helpers
  */
+
+ // Helpers for adminLocation template
 Template.adminLocation.helpers({
   imageEntries: function() {
 
@@ -10,31 +12,161 @@ Template.adminLocation.helpers({
 
 });
 
+// Helpers for adminImageEntry template
 Template.adminImageEntry.helpers({
 
   entry: function() {
     return Images.findOne(this._id);
   },
 
-  thumbPath : function() {
+  thumbPath: function() {
     return '/images/thumbnails/' + this._id + '.jpg';
   },
 
 });
 
-/*
-_id: "ds02040"
-appDate: "date-September 2014, 20"
-creationPlace: "Laguna Campground, CA"
-creditLine: "Susanne Clara Bard"
-dsLocId: 2
-expandedAspectRatio: 1.28866
-expandedHeight: 1164
-expandedWidth: 1500
-isoDate: "iso-20-09-2014"
-labelTextEnglish: "Dripping water faucets at Laguna Campground provide a precious oasis for mountain birds like this female Acorn Woodpecker."
-labelTextSpanish: "Los grifos que gotean en el campamento de Laguna proveen un invaluable oasis para los pájaros de la montaña como este carpintero bellotero hembra."
-thumbHeight: 136
-thumbWidth: 175
-title: "Female Acorn Woodpecker"
-*/
+// Helpers for adminEditModal template
+Template.adminEditModal.helpers({
+
+  editImage: function() {
+
+    return Images.findOne({ _id: Session.get('adminCurrentImageId') });
+
+  },
+
+  updateOrInsertMode: function() {
+
+    if (Session.get('adminCurrentImageId') && Session.get('adminCurrentImageId') !== '') {
+      return 'update';
+    } else {
+      return 'insert';
+    }
+
+  },
+
+  modalHeader: function() {
+
+    if (Session.get('adminCurrentImageId') && Session.get('adminCurrentImageId') !== '') {
+      return 'EDIT ' + Session.get('adminCurrentImageId');
+    } else {
+      return 'ADD';
+    }
+
+  },
+
+});
+
+Template.adminLocation.events({
+  /**
+   * Entry click
+   */
+  'click .imageList .btn':function(e) {
+
+    var clickedId = $(e.currentTarget).attr('id');
+
+    console.log(clickedId);
+
+    if (clickedId === 'addBtn') {
+
+      // TODO = show add new image form
+
+    } else {
+
+      Session.set('adminCurrentImageId', clickedId);
+
+      var imgDoc = Images.findOne({_id:Session.get('adminCurrentImageId')});
+      console.log(imgDoc.title);
+
+      // TODO = Show edit image form populated with data.
+      $('#editModal').modal('show');
+
+    }
+
+  },
+
+});
+
+/**
+ * Hooks for autoform. Manipulate data before/after submission.
+ */
+AutoForm.hooks({
+
+  form_imageEntry: {
+
+    before: {
+      // Replace `formType` with the form `type` attribute to which this hook applies
+      insert: function(doc) {
+
+        // Alter the doc to include
+        // Todo - assign adminLocationation id
+
+        // Then return it or pass it to this.result()
+        return doc;
+
+        //this.result(doc); (asynchronous)
+
+      },
+
+      update: function(doc) {
+
+        // Alter the doc as needed
+        console.log('update doc', doc);
+
+        // Then return it or pass it to this.result()
+        return doc;
+
+        //this.result(doc); (asynchronous)
+
+      },
+    },
+
+    docToForm: function(doc, ss) {
+      console.log('doc', doc);
+      console.log('ss', ss);
+
+      console.log(doc.isoDate);
+
+      // Date strings are formatted like this: iso-2014-00-00
+      // So we must break it apart
+      // var split = doc.isoDate.split('-');
+      // var year = split[1];
+      // var month = split[2];
+      // var day = split[3];
+
+      // doc.date.year = year;
+      // doc.date.month = month;
+      // doc.date.day = day;
+
+      // console.log(split);
+
+      return doc;
+
+    },
+
+    // Called when any submit operation succeeds
+    onSuccess: function(formType, result) {
+
+      console.log('autoform success:', formType, result);
+
+      var flipbookId = result;
+
+      // Add new flipbook to corresponding project
+      var projectId = Session.get('activeProjectId');
+
+      Projects.update({_id: projectId}, {$addToSet: {flipbookIds: flipbookId}});
+
+      // Hide modal
+      $('#add_flipbook').modal('hide');
+      $('#add_flipbook form')[0].reset();
+
+    },
+
+    // Called when any submit operation fails
+    onError: function(formType, error) {
+      console.log('autoform error:', formType, error);
+
+    },
+
+  },
+
+});
--- meteor/models/images.js
+++ meteor/models/images.js
@@ -9,29 +9,120 @@ Images = new Meteor.Collection('images');
 // Allow/Deny
 
 Images.allow({
-    insert: function(userId, doc){
-        return can.createImage(userId);
-    },
-    update:  function(userId, doc, fieldNames, modifier){
-        return can.editImage(userId, doc);
-    },
-    remove:  function(userId, doc){
-        return can.removeImage(userId, doc);
-    }
+  insert: function(userId, doc) {
+    return can.createImage(userId);
+  },
+
+  update:  function(userId, doc, fieldNames, modifier) {
+    return can.editImage(userId, doc);
+  },
+
+  remove:  function(userId, doc) {
+    return can.removeImage(userId, doc);
+  },
 });
 
 // Methods
 
 Meteor.methods({
-    createImage: function(image){
-        if(can.createImage(Meteor.user()))
-            Images.insert(image);
-    },
-    removeImage: function(image){
-        if(can.removeImage(Meteor.user(), image)){
-            Images.remove(image._id);
-        }else{
-            throw new Meteor.Error(403, 'You do not have the rights to delete this image.')
-        }
+  createImage: function(image) {
+    if (can.createImage(Meteor.user()))
+        Images.insert(image);
+  },
+
+  removeImage: function(image) {
+    if (can.removeImage(Meteor.user(), image)) {
+      Images.remove(image._id);
+    }else {
+      throw new Meteor.Error(403, 'You do not have the rights to delete this image.')
     }
+  },
 });
+
+// Attach schema for autoforms
+
+Images.attachSchema(new SimpleSchema({
+  title: {
+    type: String,
+    label: 'Title',
+    defaultValue: '',
+    optional: true,
+  },
+  creationPlace: {
+    type: String,
+    label: 'Creation place',
+    defaultValue: '',
+    optional: true,
+  },
+  creditLine: {
+    type: String,
+    label: 'Credit line',
+    defaultValue: '',
+    optional: true,
+  },
+  labelTextEnglish: {
+    type: String,
+    label: 'English label',
+    defaultValue: '',
+    optional: true,
+    autoform: {
+      rows: 3,
+    },
+  },
+  labelTextSpanish: {
+    type: String,
+    label: 'Spanish label',
+    defaultValue: '',
+    optional: true,
+    autoform: {
+      rows: 3,
+    },
+  },
+  formYear: {
+    type: Number,
+    label: 'Year (Required)',
+    defaultValue: '0000',
+    optional: false,
+    autoform: {
+      class: 'col-sm-6'
+    },
+  },
+  formMonth: {
+    type: Number,
+    label: 'Month',
+    defaultValue: '',
+    optional: true,
+    autoform: {
+      class: 'col-sm-6'
+    },
+  },
+  formDay: {
+    type: Number,
+    label: 'Day',
+    defaultValue: '',
+    optional: true,
+    autoform: {
+      class: 'col-sm-6'
+    },
+  },
+
+
+  // image: {
+  //   type: String,
+  //   optional: true,
+  //   autoform: {
+  //     afFieldInput: {
+  //       type: 'fileUpload',
+  //       collection: 'imageFiles',
+  //       accept: 'image/*',
+  //       label: 'Image',
+  //       removeLabel: 'Change',
+  //     },
+  //   },
+  // },
+  // isoDate: {
+  //   type: Date,
+  //   label: 'Date',
+  // },
+
+}));
