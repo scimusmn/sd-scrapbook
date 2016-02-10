a201bd92fdcd6f541ccbcc29d062d8930ef72200
Trygve <tnordberg@smm.org>
2016-02-10 16:05:18 -0600

    Moved basicAuth to server folder

--- meteor/server/basicAuth.js
+++ meteor/server/basicAuth.js
@@ -1,2 +1,5 @@
+// Configure Basic Auth to require password for any admin page.
+// Set correct name and password in config/settings.json
+
 var basicAuth = new HttpBasicAuth(Meteor.settings.public.adminName, Meteor.settings.public.adminPassword);
 basicAuth.protect(['/admin']);
